4ace9dcd1324e77304d3b82719fe019d3a2f92a2
mumuniazeez <mumuniazeez99@gmail.com>
2026-07-19 09:15:56 +0100

    Refactor code structure for improved readability and maintainability

--- src/main.js
+++ src/main.js
@@ -1,11 +1,83 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+import * as THREE from "three";
 
-`
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const render = new THREE.WebGLRenderer({
+  canvas: document.getElementById("bg"),
+});
+
+render.setPixelRatio(window.devicePixelRatio);
+
+render.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+render.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 6, 100);
+
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+
+pointLight.position.set(5, 0, 20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, render.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  render.render(scene, camera);
+}
+
+animate();
